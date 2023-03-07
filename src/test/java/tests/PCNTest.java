0651147ae2816d64a27fb7fa340ea7d1e66ac75a
Z2Data <hafez.elhawary@z2dataeg.com>
2023-03-07 14:38:46 +0200

    commit

--- src/test/java/tests/PCNTest.java
+++ src/test/java/tests/PCNTest.java
@@ -30,6 +30,5 @@ public class PCNTest extends TestBase{
         String welcomeMsg = landingPage.getWelcomeMsgTxt();
         softAssert.assertTrue(welcomeMsg.contains("Welcome"));
         softAssert.assertAll();
-        Thread.sleep(3000);
     }
 }
